88e4257a517d82914956ea18715e1c2a0adc4da2
AlexAngel102 <61982160+AlexAngel102@users.noreply.github.com>
2020-08-17 12:54:27 +0300

    Adding form for download page

--- html.php
+++ html.php
@@ -1,19 +1,25 @@
 <?php
 $html = '<!DOCTYPE html>
 <html lang="en">';
+
 $head = '<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">    
-    <title>OnlineShop.com</title>';
+    <title></title>';
+
 $clsHead = '</head>';
+
 $body = '<body>';
+
 $clsBody = '<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src = "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity = "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin = "anonymous" ></script >
 <script src = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity = "sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin = "anonymous" ></script >
 </body>';
+
 $clsHtml = '</html>';
+
 $navBar = '<nav class="navbar navbar-light bg-dark stiky-top">
         <a href="index.php" class="navbar-brand "><h4 class="text-white">ShopOnline.com</h4></a>
         <form class="form-inline">
@@ -21,6 +27,7 @@ $navBar = '<nav class="navbar navbar-light bg-dark stiky-top">
             <button class="btn btn-outline-success my-2 my-sm-0 mx-1" type="submit">Register</button>
         </form>
         </nav>';
+
 $itemsTableHead = '<div class="col-9">
             <table class="table">
                 <thead><tr><th><p style="font-size:25pt" align="center">List of items</p></th></tr></thead>
@@ -33,10 +40,12 @@ $itemsTableHead = '<div class="col-9">
                 </tr>
                 </thead>
                 <tbody>';
+
 $itemsTableEnd = ' </tbody>
             </table>
         </div>
     </div>';
+
 $twoColumsTemplate = '<div class="row row-cols-2">';
 
 $loginForm = '<div class="col-3">
@@ -62,4 +71,13 @@ $loginForm = '<div class="col-3">
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="#">Login in</a>
            </div>';
+
 $clsDiv = '</div>';
+
+$upForm = '
+<form class="form-control" action="index.php" method="post">
+<div class="custom-file">
+  <input type="file" class="custom-file-input" id="customFileLang" lang="ru">
+  <label class="custom-file-label" for="customFileLang">Выберите файл</label>
+</div>
+</form>';
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'html.php';
+
+echo $html;
+echo $head;
+echo $body;
+
+echo $upForm;
+
+echo $clsBody;
+echo $clsHead;
+echo $clsHtml;
